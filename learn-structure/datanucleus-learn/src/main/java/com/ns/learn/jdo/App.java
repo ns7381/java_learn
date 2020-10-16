dbffa9a53ec25f59fac6444251ce0f578c17d8c5
ns7381 <ns7381@sina.cn>
2020-10-16 18:04:46 +0800

    datanucleus-learn

---
+++ learn-structure/datanucleus-learn/src/main/java/com/ns/learn/jdo/App.java
@@ -0,0 +1,58 @@
+package com.ns.learn.jdo;
+
+
+import com.ns.learn.jdo.model.Inventory;
+import com.ns.learn.jdo.model.Product;
+
+import javax.jdo.*;
+import java.util.List;
+import java.util.Properties;
+
+public class App {
+    public PersistenceManagerFactory getPF() {
+        Properties properties = new Properties();
+        properties.setProperty("javax.jdo.PersistenceManagerFactoryClass", "org.datanucleus.api.jdo.JDOPersistenceManagerFactory");
+        properties.setProperty("datanucleus.schema.autoCreateAll", "true");
+        properties.setProperty("javax.jdo.option.ConnectionDriverName", "com.mysql.jdbc.Driver");
+        properties.setProperty("javax.jdo.option.ConnectionURL", "jdbc:mysql://localhost:3306/jdotest?useSSL=false");
+        properties.setProperty("javax.jdo.option.ConnectionUserName", "root");
+        properties.setProperty("javax.jdo.option.ConnectionPassword", "root");
+        return JDOHelper.getPersistenceManagerFactory(properties);
+    }
+
+    public PersistenceManagerFactory getPFFromXML() {
+        return JDOHelper.getPersistenceManagerFactory("Tutorial");
+    }
+
+    public void insert() {
+        try (PersistenceManager pm = getPF().getPersistenceManager()) {
+            Transaction tx = pm.currentTransaction();
+            try {
+                tx.begin();
+                Inventory inv = new Inventory("My Inventory2");
+                Product product = new Product("Sony Discman2", "A standard discman from Sony", 49.99);
+                inv.getProducts().add(product);
+                pm.makePersistent(inv);
+                tx.commit();
+            } finally {
+                if (tx.isActive()) {
+                    tx.rollback();
+                }
+            }
+        }
+    }
+
+    public void select() {
+        Query q = getPF().getPersistenceManager().newQuery(
+                "SELECT FROM " + Inventory.class.getName());
+        List<Inventory> inventories = (List<Inventory>) q.execute();
+        for (Inventory p : inventories) {
+            System.out.println(p);
+        }
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.select();
+    }
+}
---
+++ learn-structure/datanucleus-learn/src/main/java/com/ns/learn/jdo/model/Product.java
@@ -0,0 +1,40 @@
+package com.ns.learn.jdo.model;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import java.util.Objects;
+
+@PersistenceCapable
+public class Product {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.INCREMENT)
+    long id;
+    String name = null;
+    String description = null;
+    double price = 0.0;
+
+    public Product(String name, String desc, double price) {
+        this.name = name;
+        this.description = desc;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Double.compare(product.price, price) == 0 &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, price);
+    }
+
+}
